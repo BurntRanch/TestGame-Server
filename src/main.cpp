ce3201166b4742094724a4f1b53c68725abbb6b7
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2024-12-12 13:03:27 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,11 @@
 #include <fmt/ranges.h>
 #include <stdexcept>
 #include <string>
+#include <thread>
 #include "camera.hpp"
 #include "common.hpp"
 #include "model.hpp"
+#include "steamnetworkingtypes.h"
 #include "ui/arrows.hpp"
 #include "ui/button.hpp"
 #include "ui/label.hpp"
@@ -27,54 +29,22 @@
 #include "rapidxml_print.hpp"
 #include "tinyfiledialogs.h"
 
-Camera cam(glm::vec3(-1.0f, 0.0f, 0.0f));
-Settings settings("settings.toml");
 std::unique_ptr<Engine> engine;
 
 namespace State {
 }
 
-float lastX, lastY;
-Uint32 lastMouseState;
-
-void Update() {
-    //fmt::println("Hi!");
-}
-
-void FixedUpdate(const std::array<bool, 322> &keyMap) {
-
-}
-
-void onButtonClick(std::string id) {
-    if (id == "playButton") {
-        fmt::println("*plays*");
-
-        engine->ImportScene("viking_room.xml");
-
-        UI::GenericElement *mainMenuGroup = engine->GetElementByID("mainMenuGroup");
-
-        UI::GenericElement *hudGroup = engine->GetElementByID("hudGroup");
-
-        if (mainMenuGroup && mainMenuGroup->type == UI::SCALABLE) {
-            mainMenuGroup->SetVisible(false);
-            hudGroup->SetVisible(true);
-        }
-    }
-}
-
 int main() {
-    cam.MouseSensitivity = settings.MouseSensitivity;
-    cam.FOV = settings.FieldOfView;
-    cam.MovementSpeed = settings.Velocity;
-
     engine = std::make_unique<Engine>();
     
-    try {
-        
-    } catch(const std::runtime_error &e) {
-        fmt::println("Exception has occurred: {}", e.what());
-        return -1;
-    }
+    engine->InitNetworking();
+
+    SteamNetworkingIPAddr ipAddr;
+    ipAddr.SetIPv6LocalHost(9582);
+
+    engine->HostGameServer(ipAddr);
+
+    while (true) { std::this_thread::sleep_for(std::chrono::hours(90)); };
 
     return 0;
 }
